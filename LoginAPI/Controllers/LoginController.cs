31ecc7b2162f4e5544f807a3cc0fb41598cb3475
Barış Bükümcüler <baris_bukumculer@hotmail.com>
2023-07-20 23:02:12 +0300

    Add project files.

---
+++ LoginAPI/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using LoginAPI.Models;
+using LoginAPI.Security;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpPost]
+        public IActionResult GetToken(UserViewModel model)
+        {
+            var username = _configuration["Token:Username"];
+            var password = _configuration["Token:Password"];
+            if (model.Username == username && model.Password == password)
+            {
+                Token token = TokenHandler.CreateToken(_configuration);
+                
+                return Ok(new
+                {
+                    userid=1,
+                    Username=username,
+                    Password=password,
+                    Token = token
+                });
+            }
+            else
+            {
+                return BadRequest(new { Message = "Hatalı kullanıcı adı veya şifre girdiniz. Tekrar Deneyiniz." });
+            }
+        }
+    }
+}
---
+++ LoginAPI/Models/UserViewModel.cs
@@ -0,0 +1,10 @@
+﻿using LoginAPI.Security;
+
+namespace LoginAPI.Models
+{
+    public class UserViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
